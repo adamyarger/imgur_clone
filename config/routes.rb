3f6b1f245f4cc8aaeb8252a93f7db18263139a7a
adamyarger <yargeradam@gmail.com>
2015-06-05 22:32:39 -0500

    adds show action

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   devise_for :users, :controllers => { registrations: 'registrations' }
   root 'posts#index'
 
-  resources :posts, only: [:new, :create] 
+  resources :posts, only: [:new, :create, :show] 
 end
