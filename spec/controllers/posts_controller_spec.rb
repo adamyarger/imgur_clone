47a98775468f91548ce5bdaae28d323fe614cd76
adamyarger <yargeradam@gmail.com>
2015-06-06 10:52:49 -0500

    adds show test

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -51,6 +51,20 @@ describe PostsController, type: :controller do
 			expect(response).to render_template :index
 		end
 	end
+
+	describe 'GET #show' do
+		it 'assigns the correct post' do
+			post = FactoryGirl.create :post
+			get :show, id: post
+			expect(assigns(:post)).to eq(post)
+		end
+
+		it 'renders the #show view' do
+			post = FactoryGirl.create :post
+			get :show, id: post
+			expect(response).to render_template :show
+		end
+	end
 end
 
 
