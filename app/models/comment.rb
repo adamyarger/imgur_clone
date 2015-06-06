a9152405d6ed3fb092499053261f689ecb2e9000
adamyarger <yargeradam@gmail.com>
2015-06-06 18:16:32 -0500

    adds passing integration test

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,8 @@
 class Comment < ActiveRecord::Base
 	belongs_to :user
 	belongs_to :post
+    validates :body, presence: true
+    # validates :title, :image_url, :presence => true
 
 	default_scope -> {order(created_at: :desc)}
 
