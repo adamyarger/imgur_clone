487cc1f85c96744c526a82ce779a021b6593716b
adamyarger <yargeradam@gmail.com>
2015-06-07 10:25:36 -0500

    adds passing create action for comments

--- spec/controllers/comments_controller_spec.rb
+++ spec/controllers/comments_controller_spec.rb
@@ -1,28 +1,33 @@
-# require 'rails_helper'
+require 'rails_helper'
 
-# describe CommentsController, type: :controller do
+describe CommentsController, type: :controller do
 	
-# 	describe 'POST #create' do
-# 		context 'with valid attributes' do
-# 			before(:each) do
-# 				user = FactoryGirl.create :user
-# 				sign_in user
+	describe 'POST #create' do
+		context 'with valid attributes' do
+			before(:each) do
+				@user = FactoryGirl.create :user
+				@post = FactoryGirl.create :post
+				sign_in @user
+				@comment_attributes = FactoryGirl.attributes_for :comment
+			end
 
-# 				post = FactoryGirl.create :post
-				
-# 			end
+			it 'creates the comment' do
+				post :create, { user_id: @user.id, post_id: @post, comment: @comment_attributes }
+				expect(Comment.count).to eq(1)
+			end
 
-# 			it 'creates the comment' do
-# 				post :create, comment: attributes_for(:comment)
-# 				expect(Comment.count).to eq(1)
-# 			end
-# 		end
-# 	end
-# end
+			# it "renders the comment" do
+   #      product_response = json_response[:product]
+   #      expect(product_response[:title]).to eql @product_attributes[:title]
+   #    end
 
-# # before(:each) do
-# #     @thing = Factory(:thing)
-# #     attrs = FactoryGirl.attributes_for(:description, :thing_id => @thing)
+		end
+	end
+end
 
-# #     post :create, :thing_id => @thing, :description => attrs
-# #   end
+# before(:each) do
+#     @thing = Factory(:thing)
+#     attrs = FactoryGirl.attributes_for(:description, :thing_id => @thing)
+
+#     post :create, :thing_id => @thing, :description => attrs
+#   end
